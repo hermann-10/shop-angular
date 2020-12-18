4932ece90239a46058dd74c356be74ddad328e4d
hermannmanuel <hermann.manuel>
2020-12-18 13:24:09 +0100

    Configuration de la navigation du site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,21 @@ import { ShopComponent } from './shop/shop.component';
 import { ProductsComponent } from './shop/products/products.component';
 import { SingleProductComponent } from './shop/single-product/single-product.component';
 import { CartComponent } from './shop/cart/cart.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'shop', component: ShopComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'single-product/:id', component: SingleProductComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'notFound', component: NotFoundComponent},
+  {path: '', component: ShopComponent},
+  {path: '**', redirectTo: 'notFound', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -26,11 +41,13 @@ import { CartComponent } from './shop/cart/cart.component';
     ShopComponent,
     ProductsComponent,
     SingleProductComponent,
-    CartComponent
+    CartComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
